4b31956386b65993ebb5197091c3201c821861db
joacoarana06 <joacoarana06@gmail.com>
2025-08-11 08:38:32 -0300

    Agregar funcionalidad para detectar y almacenar consultas de búsqueda en Google, y diferencias entre paginas cargadas y pestañas

---
+++ background.js
@@ -0,0 +1,8 @@
+// Este es el punto de entrada principal para todos los scripts de background.
+// Importa y ejecuta los diferentes módulos de funcionalidades.
+
+import { initializeDetectarBusqueda } from './background/detectar_busqueda.js';
+
+initializeDetectarBusqueda();
+
+
